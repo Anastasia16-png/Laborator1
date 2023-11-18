4fe8eb2993740c807cc4460c1ed524188c4ed108
Anastasia16-png <114298605+Anastasia16-png@users.noreply.github.com>
2023-11-18 16:28:29 +0200

    Update camera.js

--- camera.js
+++ camera.js
@@ -1,5 +1,5 @@
 var video = document.getElementById("video");
-//
+
 function on_cam_success(stream) //function that will access the camera and transmit  the  video  to  the  camera
 {
 	video.srcObject = stream;
@@ -8,12 +8,12 @@ function on_cam_error(err) //for transmitting an error in case we cannot access 
 {
 	alert("error." +err.message);
 }
-// 
+
 var constraints = { audio: false, video: true }; 
 navigator.mediaDevices.getUserMedia(constraints) // method prompts  the  user  for  permission  to  use  a  video input device and an audio input device as the source 
 .then(on_cam_success)
 .catch(on_cam_error);
-//
+
 function capteaza()
 {
 	var c = document.getElementById("canvas"); // method  returns  the  element  that  has  the ID attribute with the specified value, that is, the video id
@@ -22,4 +22,5 @@ function capteaza()
 	var ctx = c.getContext("2d"); 
 	ctx.drawImage(video, 0, 0, 640, 480); //display point starting by setting the x axis and the y axis
 }
-	//
+video.addEventListener("touchstart",capteaza); 
+video.addEventListener("mousedown",capteaza);
